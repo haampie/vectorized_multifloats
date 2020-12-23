6f063edb471d0b1563627c7a1ad4f10fa99f0391
Harmen Stoppels <harmenstoppels@gmail.com>
2020-12-23 17:12:03 +0100

    more trial and error

--- example.jl
+++ example.jl
@@ -1,32 +1,44 @@
 using MultiFloats, VectorizationBase
-
 using VectorizationBase: extractelement, pick_vector_width
 using MultiFloats: renormalize
-
+using VectorizationBase: VecUnroll, Unroll, vtranspose, unrolleddata, vload
 using DoubleFloats
+using BenchmarkTools
+using Random: AbstractRNG, SamplerType
+using Base: IEEEFloat
 
 import DoubleFloats: DoubleFloat
+import Random: rand
+import LinearAlgebra: dot, axpy!, norm
+import Base: sum
 
-# struct of array type.
-struct MultiFloatSoA{T,N}
-    limbs::NTuple{N,Vector{T}}
-end
 
-function MultiFloatSoA(xs::Vector{MultiFloat{T,N}}) where {T,N}
-    limbs = ntuple(_ -> Vector{T}(undef, length(xs)), Val{N}())
+using StructArrays, MultiFloats
 
-    for i in eachindex(xs)
-        for j = 1:N
-            limbs[j][i] = xs[i]._limbs[j]
-        end
+import StructArrays: staticschema, createinstance
+import Base: getproperty, propertynames
+
+propertynames(::MultiFloat{T,N}) where {T,N} = ntuple(i -> Symbol(:idx_, i), Val{N}())
+
+@generated function getproperty(x::MultiFloat{T,N}, s::Symbol) where {N,T}
+    symbols = [Symbol(:idx_, i) for i = 1:N]
+    quote
+        $(Expr(:meta, :inline))
+        $([:(s === $(QuoteNode(symbols[i])) && return getfield(x, 1)[$i]) for i = 1:N]...)
+        return getfield(x, s)
     end
-
-    return MultiFloatSoA(limbs)
 end
+
+@generated function staticschema(::Type{MultiFloat{T,N}}) where {N,T}
+    symbols = [Symbol(:idx_, i) for i = 1:N]
+    quote
+        NamedTuple{$(QuoteNode(ntuple(i -> symbols[i], Val{N}()))), NTuple{$N,$T}}
+    end
+end
+
+createinstance(::Type{MultiFloat{T,N}}, args...) where {N,T} = MultiFloat{T,N}(values(args))
 
 DoubleFloat(x::MultiFloat{T,2}) where {T} = DoubleFloat{T}(x._limbs[1], x._limbs[2])
-
-using VectorizationBase: VecUnroll, vtranspose, unrolleddata
 
 @generated function MultiFloatOfVec(fs::NTuple{M,MultiFloat{T,N}}) where {T,M,N}
     exprs = [:(Vec($([:(fs[$j]._limbs[$i]) for j=1:M]...))) for i=1:N]
@@ -47,15 +59,28 @@ end
 
 function trivial_sum(xs)
     t = zero(eltype(xs))
-    @inbounds @simd for x in xs
+    @inbounds @simd ivdep for x in xs
         t += x
     end
     return t
 end
 
-using VectorizationBase: Unroll, vload, vtranspose, unrolleddata
+function trivial_dot(xs, ys)
+    t = zero(eltype(xs))
+    @inbounds @simd ivdep for i = 1:length(xs)
+        t += xs[i] * ys[i]
+    end
+    return t
+end
 
-@generated function vectorized_sum(xs::AbstractArray{MultiFloat{T,N}}) where {T,N}
+function trivial_axpy!(a, xs, ys)
+    @inbounds @simd ivdep for i = 1:length(xs)
+        ys[i] = xs[i] * a
+    end
+    return ys
+end
+
+@generated function sum(xs::AbstractArray{MultiFloat{T,N}}) where {T,N}
     M = pick_vector_width(T)
 
     load_xs = ntuple(k -> :(xs[i + $(k - 1)]), M)
@@ -84,52 +109,7 @@ using VectorizationBase: Unroll, vload, vtranspose, unrolleddata
     end
 end
 
-@generated function handwritten_sum(xs::MultiFloatSoA{T,N}) where {T,N}
-    M = pick_vector_width(T)
-
-    quote
-        len = length(xs.limbs[1])
-        t = zero(MultiFloat{Vec{$M,$T},$N})
-
-        # create N pointers
-        Base.Cartesian.@nexprs $N i -> px_i = stridedpointer(xs.limbs[i])
-        
-        j = 1
-
-        while j < len
-            # N loads.
-            t += MultiFloat(Base.Cartesian.@ntuple $N i -> vload(px_i, (MM{$M}(j),)))
-            j += $M
-        end
-
-        return +(TupleOfMultiFloat(t)...)
-    end
-end
-
-function handwritten_sum(xs::Vector{MultiFloat{T,N}}) where {T,N}
-    M = pick_vector_width(T)
-
-    t = zero(MultiFloat{Vec{M,T},N})
-
-    p = stridedpointer(reinterpret(T, xs))
-
-    for j = Base.OneTo(length(xs)÷M)
-        idx = (j - 1) * M * N + 1
-        t += MultiFloat(unrolleddata(vtranspose(vload(p, Unroll{1,1,N,1,M,0x0000000000000000}((idx,))), Val{N}())))
-    end
-
-    return +(TupleOfMultiFloat(t)...)
-end
-
-function trivial_dot(xs, ys)
-    t = zero(eltype(xs))
-    @inbounds for i = 1:length(xs)
-        t += xs[i] * ys[i]
-    end
-    return t
-end
-
-@generated function vectorized_dot(xs::AbstractArray{MultiFloat{T,N}}, ys::AbstractArray{MultiFloat{T,N}}) where {T,N}
+@generated function dot(xs::AbstractArray{MultiFloat{T,N}}, ys::AbstractArray{MultiFloat{T,N}}) where {T,N}
     M = pick_vector_width(T)
 
     load_xs = ntuple(k -> :(xs[i + $(k - 1)]), M)
@@ -159,108 +139,75 @@ end
     end
 end
 
-@generated function handwritten_dot(xs::MultiFloatSoA{T,N}, ys::MultiFloatSoA{T,N}) where {T,N}
+@generated function axpy!(a::MultiFloat{T,N}, xs::AbstractArray{MultiFloat{T,N}}, ys::AbstractArray{MultiFloat{T,N}}) where {T,N}
     M = pick_vector_width(T)
+
+    load_xs = ntuple(k -> :(xs[i + $(k - 1)]), M)
+    load_ys = ntuple(k -> :(ys[i + $(k - 1)]), M)
+    assign = [:($(load_ys[i]) = t[$i]) for i = 1:M]
+    broadcast_a = ntuple(i -> :a, M)
 
     quote
-        len = length(xs.limbs[1])
-        t = zero(MultiFloat{Vec{$M,$T},$N})
-
-        # create N pointers
-        Base.Cartesian.@nexprs $N i -> px_i = stridedpointer(xs.limbs[i])
-        Base.Cartesian.@nexprs $N i -> py_i = stridedpointer(ys.limbs[i])
-        
-        j = 1
-
-        while j < len
-            # N loads.
-            x = MultiFloat(Base.Cartesian.@ntuple $N i -> vload(px_i, (MM{$M}(j),)))
-            y = MultiFloat(Base.Cartesian.@ntuple $N i -> vload(py_i, (MM{$M}(j),)))
-            t += x * y
-            j += $M
+        # iterate in steps of M
+        step, i = 1, 1
+        av = MultiFloatOfVec(tuple($(broadcast_a...)))
+        @inbounds while step ≤ length(xs) ÷ $M
+            x = MultiFloatOfVec(tuple($(load_xs...)))
+            y = MultiFloatOfVec(tuple($(load_ys...)))
+            result = y + x * av
+            t = TupleOfMultiFloat(result)
+            $(assign...)
+            step += 1
+            i += $M
+        end
+    
+        # sum the remainder
+        while i ≤ length(xs)
+            @inbounds ys[i] += xs[i] * a
+            i += 1
         end
 
-        return +(TupleOfMultiFloat(t)...)
+        return ys
     end
 end
 
-function handwritten_dot(xs::Vector{MultiFloat{T,N}}, ys::Vector{MultiFloat{T,N}}) where {T,N}
-    M = pick_vector_width(T)
+norm(xs::AbstractArray{<:MultiFloat}) = sqrt(dot(xs, xs))
 
-    t = zero(MultiFloat{Vec{M,T},N})
-
-    px = stridedpointer(reinterpret(T, xs))
-    py = stridedpointer(reinterpret(T, ys))
-
-    # load M Multifloats at a time.
-    for j = Base.OneTo(length(xs)÷M)
-        idx = (j - 1) * M * N + 1
-        mx = MultiFloat(unrolleddata(vtranspose(vload(px, Unroll{1,1,N,1,M,0x0000000000000000}((idx,))), Val{N}())))
-        my = MultiFloat(unrolleddata(vtranspose(vload(py, Unroll{1,1,N,1,M,0x0000000000000000}((idx,))), Val{N}())))
-        
-        t += mx * my
-    end
-
-    return +(TupleOfMultiFloat(t)...)
-end
-
-using BenchmarkTools
-
-random_vec(::Type{MultiFloat{T,N}}, k) where {T,N} =
-    [renormalize(MultiFloat(ntuple(i -> rand(T) * eps(T)^(i-1), N))) for _ = 1:k]
+rand(rng::AbstractRNG, ::SamplerType{MultiFloat{T,N}}) where {T<:IEEEFloat,N} =
+    renormalize(MultiFloat(ntuple(i -> rand(T) * eps(T)^(i-1), Val{N}())))
 
 function benchmark_dot(::Type{T}) where {T<:MultiFloat}
-    xs = random_vec(T, 2^13)
-    ys = random_vec(T, 2^13)
+    xs, ys = rand(T, 2^13), rand(T, 2^13)
+    xs_soa, ys_soa = StructArray(xs), StructArray(ys)
 
-    xs_soa = StructArray(xs)
-    ys_soa = StructArray(ys)
+    @show dot(xs, ys) - trivial_dot(xs, ys)
 
-    @show handwritten_dot(xs, ys) - trivial_dot(xs, ys)
-    @show vectorized_dot(xs_soa, ys_soa) - trivial_dot(xs, ys)
-
-    handwritten = @benchmark handwritten_dot($xs, $ys)
-    vectorized = @benchmark vectorized_dot($xs_soa, $ys_soa)
+    vectorized = @benchmark dot($xs_soa, $ys_soa)
     trivial = @benchmark trivial_dot($xs, $ys)
 
-    return handwritten, vectorized, trivial
+    return vectorized, trivial
+end
+
+function benchmark_axpy(::Type{T}) where {T<:MultiFloat}
+    xs, ys = rand(T, 2^13), rand(T, 2^13)
+    xs_soa, ys_soa = StructArray(xs), StructArray(ys)
+
+    @show norm(axpy!(T(2.0), xs, ys) - trivial_axpy!(T(2.0), xs, ys))
+
+    vectorized = @benchmark axpy!(a, xs_soa, ys_soa) setup=(a=$T(1.0); xs_soa=StructArray(rand($T, 2^13)); ys_soa=StructArray(rand($T, 2^13)))
+    trivial = @benchmark trivial_axpy!(a, xs, ys) setup=(a=$T(1.0); xs=rand($T, 2^13); ys=rand($T, 2^13))
+
+    return vectorized, trivial
 end
 
 function benchmark_sum(::Type{T}) where {T<:MultiFloat}
-    xs = random_vec(T, 2^13)
+    xs = rand(T, 2^13)
     xs_soa = StructArray(xs)
 
-    @show handwritten_sum(xs) - trivial_sum(xs)
-    @show vectorized_sum(xs_soa) - trivial_sum(xs)
+    @show sum(xs_soa) - trivial_sum(xs)
 
-    handwritten = @benchmark handwritten_sum($xs)
-    vectorized = @benchmark vectorized_sum($xs_soa)
+    vectorized = @benchmark sum($xs_soa)
     trivial = @benchmark trivial_sum($xs)
 
-    return handwritten, vectorized, trivial
+    return vectorized, trivial
 end
-
-using StructArrays, MultiFloats
-
-import StructArrays: staticschema, createinstance
-import Base: getproperty, propertynames
-
-propertynames(::MultiFloat{T,N}) where {T,N} = ntuple(i -> Symbol(:idx_, i), Val{N}())
-
-@generated function getproperty(x::MultiFloat{T,N}, s::Symbol) where {N,T}
-    symbols = [Symbol(:idx_, i) for i = 1:N]
-    quote
-        $(Expr(:meta, :inline))
-        $([:(s === $(QuoteNode(symbols[i])) && return getfield(x, 1)[$i]) for i = 1:N]...)
-        return getfield(x, s)
-    end
-end
-
-@generated function staticschema(::Type{MultiFloat{T,N}}) where {N,T}
-    symbols = [Symbol(:idx_, i) for i = 1:N]
-    quote
-        NamedTuple{$(QuoteNode(ntuple(i -> symbols[i], Val{N}()))), NTuple{$N,$T}}
-    end
-end
-
-createinstance(::Type{MultiFloat{T,N}}, args...) where {N,T} = MultiFloat{T,N}(values(args))
